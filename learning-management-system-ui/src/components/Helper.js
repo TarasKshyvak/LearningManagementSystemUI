f951f7572240febf76270f92da9626728a837a3c
maksym-snizhynskyi <104899693+maksym-snizhynskyi@users.noreply.github.com>
2022-07-13 16:07:22 +0300

    Delete Helper.js

--- learning-management-system-ui/src/components/Helper.js
+++
@@ -1,8 +0,0 @@
-export const convertDate = (date) => {
-    date = new Date(date.toDateString());
-    let hoursDiff = date.getHours() - date.getTimezoneOffset() / 60;
-    let minutesDiff = (date.getHours() - date.getTimezoneOffset()) % 60;
-    date.setHours(hoursDiff);
-    date.setMinutes(minutesDiff);
-    return date;
-}
